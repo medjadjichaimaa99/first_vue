0ec3807a8300275466bcb1ac7f59b895810af04b
medjadjichaimaa99 <57466637+medjadjichaimaa99@users.noreply.github.com>
2020-07-23 00:10:50 +0100

    Vue.js project

---
+++ src/store/index.js
@@ -0,0 +1,12 @@
+//import Vue from 'vue'
+//import Vuex from 'vuex'
+const Vue = require('vue')
+const Vuex = require('vuex')
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+    state: {},
+    mutations: {},
+    actions: {},
+    modules: {}
+})
